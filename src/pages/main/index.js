301d161364a4a385b7f8a616ac227c049c58cfdb
Alexandr <Alexandridiyatov@gmail.com>
2020-04-23 17:43:21 +0400

    исправил названия классов по БЭМ, добавил переменные, убрал лишние компоненты

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -4,18 +4,18 @@ var buttonClose = document.querySelector(".modal-close_js");
 var input = document.querySelector(".contact-us-form__input_js");
 
 buttonOpen.addEventListener("click", function () {
-  popup.classList.remove("modal-bg_close");
+  popup.classList.remove("modal-bg_hidden");
   input.focus();
 });
 
 buttonClose.addEventListener("click", function () {
-  popup.classList.add("modal-bg_close");
+  popup.classList.add("modal-bg_hidden");
   buttonOpen.focus();
 });
 // Escape
 window.addEventListener("keydown", function (event) {
-  if(!popup.classList.contains("modal-bg_close") && event.code === "Escape"){
-    popup.classList.add("modal-bg_close");
+  if(!popup.classList.contains("modal-bg_hidden") && event.code === "Escape"){
+    popup.classList.add("modal-bg_hidden");
     buttonOpen.focus();
   }
 });
